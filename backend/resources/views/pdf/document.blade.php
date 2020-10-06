bad4b0b9ec496072f18977eeeee4843dd76fc5b9
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2020-10-07 06:30:57 +0700

    report paralegal 1

--- backend/resources/views/pdf/document.blade.php
+++ backend/resources/views/pdf/document.blade.php
@@ -3,52 +3,121 @@
 
     </head>
     <body>
-        <h1>LAPORAN KEGIATAN</h1>
+        <h3 style="text-align:center">LAPORAN KEGIATAN PARALEGAL</h3>
         <table>
             <tbody>
                 <tr>
-                    <td width="150">
+                    <td width="20" style="padding-bottom: 10px;">
+                        <strong>1.</strong>
+                    </td>
+                    <td style="padding-bottom: 10px;">
                         <strong>PARALEGAL</strong>
                     </td>
-                    <td>{{$kegiatan->name}}</td>
+                    <td style="padding-bottom: 10px;">:</td>
+                    <td style="padding-bottom: 10px;">{{$kegiatan->name}}</td>
                 </tr>
                 <tr>
-                    <td width="150">
+                    <td style="padding-bottom: 10px;">
+                        <strong>2.</strong>
+                    </td>
+                    <td style="padding-bottom: 10px;">
                         <strong>PEMOHON</strong>
                     </td>
-                    <td>{{$kegiatan->pemohon}}</td>
+                    <td style="padding-bottom: 10px;">:</td>
+                    <td style="padding-bottom: 10px;">{{$kegiatan->pemohon}}</td>
                 </tr>
                 <tr>
-                    <td>
-                        <strong>JENIS KEGIATAN</strong>
+                    <td style="padding-bottom: 10px;">
+                        <strong>3.</strong>
                     </td>
-                    <td>{{$kegiatan->nama_jenis}}</td>
-                </tr>
-                <tr>
-                    <td>
-                        <strong>NAMA KEGIATAN</strong>
+                    <td style="padding-bottom: 10px;">
+                        <strong>ALAMAT</strong>
                     </td>
-                    <td>{{$kegiatan->nama_kegiatan}}</td>
+                    <td style="padding-bottom: 10px;">:</td>
+                    <td style="padding-bottom: 10px;">{{$kegiatan->tempat}}</td>
                 </tr>
                 <tr>
-                    <td>
-                        <strong>REKOMENDASI KEGIATAN</strong>
+                    <td style="padding-bottom: 10px;">
+                        <strong>4.</strong>
                     </td>
-                    <td>{{$kegiatan->rekomendasi_kegiatan}}</td>
-                </tr>
-                <tr>
-                    <td>
-                        <strong>TEMPAT</strong>
-                    </td>
-                    <td>{{$kegiatan->tempat}}</td>
-                </tr>
-                <tr>
-                    <td>
+                    <td style="padding-bottom: 10px;">
                         <strong>TANGGAL</strong>
                     </td>
-                    <td>{{$kegiatan->tanggal}}</td>
+                    <td style="padding-bottom: 10px;">:</td>
+                    <td style="padding-bottom: 10px;">{{date('d-m-Y',strtotime($kegiatan->tanggal))}}</td>
                 </tr>
+                <tr>
+                    <td style="padding-bottom: 10px;">
+                        <strong>5.</strong>
+                    </td>
+                    <td style="padding-bottom: 10px;">
+                        <strong>WAKTU</strong>
+                    </td>
+                    <td style="padding-bottom: 10px;">:</td>
+                    <td style="padding-bottom: 10px;">{{date('H:m:s',strtotime($kegiatan->tanggal))}}</td>
+                </tr>
+                <tr>
+                    <td style="padding-bottom: 10px;">
+                        <strong>6.</strong>
+                    </td>
+                    <td style="padding-bottom: 10px;">
+                        <strong>JENIS KEGIATAN</strong>
+                    </td>
+                    <td style="padding-bottom: 10px;">:</td>
+                    <td style="padding-bottom: 10px;">{{$kegiatan->nama_jenis}}</td>
+                </tr>
+                <tr>
+                    <td style="padding-bottom: 10px;">
+                        <strong>7.</strong>
+                    </td>
+                    <td style="padding-bottom: 10px;">
+                        <strong>NAMA KEGIATAN</strong>
+                    </td>
+                    <td style="padding-bottom: 10px;">:</td>
+                    <td style="padding-bottom: 10px;">{{$kegiatan->nama_kegiatan}}</td>
+                </tr>
+                <tr>
+                    <td style="padding-bottom: 10px;">
+                        <strong>8.</strong>
+                    </td>
+                    <td style="padding-bottom: 10px;">
+                        <strong>URAIAN / KRONOLOGIS KEGIATAN</strong>
+                    </td>
+                    <td style="padding-bottom: 10px;">:</td>
+                    <td style="padding-bottom: 10px;">{{$kegiatan->uraian_kegiatan}}</td>
+                </tr>  
+                <tr>
+                    <td style="padding-bottom: 10px;">
+                        <strong>9.</strong>
+                    </td>
+                    <td style="padding-bottom: 10px;">
+                        <strong>REKOMENDASI PARALEGAL</strong>
+                    </td>
+                    <td style="padding-bottom: 10px;">:</td>
+                    <td style="padding-bottom: 10px;">{{$kegiatan->rekomendasi_kegiatan}}</td>
+                </tr>  
             </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="3"></td>
+                    <td style="text-align:center;font-weight:bold;">Kabupaten Bintan, {{date('d-m-Y',strtotime($kegiatan->updated_at))}}</td>
+                </tr>
+                <tr>
+                    <td colspan="3" style="text-align:center;font-weight:bold;padding-bottom:40px">&nbsp;</td>
+                    <td style="text-align:center;font-weight:bold;padding-bottom:40px">Paralegal</td>
+                </tr>                
+                <tr>
+                    <td colspan="3" style="text-align:center;font-weight:bold;padding-bottom:40px">&nbsp;</td>
+                    <td style="text-align:center;font-weight:bold;padding-bottom:40px">&nbsp;</td>
+                </tr>                
+                <tr>
+                    <td colspan="4" style="text-align:center;font-weight:bold;padding-left:50px">Mengetahui, </td>                    
+                </tr>                
+                <tr>
+                    <td colspan="2" style="text-align:center;font-weight:bold;">Pembina </td>                    
+                    <td colspan="2" style="text-align:center;font-weight:bold;">Kepala Desa </td>                    
+                </tr>
+            </tfoot>
         </table>        
     </body>
 </html>
